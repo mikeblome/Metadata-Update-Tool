ff55a367c1fcd4143aa3c557a23aa723d06ccb73
Colin Robertson <corob@microsoft.com>
2017-07-26 12:18:19 -0700

    Add options code, debug output of files

--- code/MUT/MUT/Program.cs
+++ code/MUT/MUT/Program.cs
@@ -10,6 +10,16 @@ namespace MUT
     {
         static void Main(string[] args)
         {
+            var opts = new Options(args);
+
+            // Debug output of recognized options
+            opts.PrintOptions();
+            var files = opts.GetFiles();
+            Console.WriteLine("Specified files:");
+            foreach (var filepath in files)
+            {
+                Console.WriteLine(filepath);
+            }
         }
     }
 }
