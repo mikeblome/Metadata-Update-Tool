e6f2c7a6b5c76a4106733473be0393a65fdfa109
Genevieve Warren <gewarren@microsoft.com>
2017-09-22 17:05:28 -0700

    Changed namespaces and added a bit of output logging.

--- code/MUT/MUT/Tag.cs
+++ code/MUT/MUT/Tag.cs
@@ -1,4 +1,4 @@
-﻿namespace MUT
+﻿namespace MdExtract
 {
     using System;
     using System.Collections.Generic;
--- code/MUT/mdapply/Command.cs
+++ code/MUT/mdapply/Command.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace mdapply
+namespace MdApply
 {
-    using MUT;
+    using MdExtract;
 
     class Command
     {
